cd5d344f78879d7911087eb03b916899b2708dbb
Jamie Joahill <jamie.joahill@gmail.com>
2021-10-11 11:27:50 +0100

    JS - Random shape generated/autoMoveDown & can move around grid

--- scripts/app.js
+++ scripts/app.js
@@ -30,35 +30,58 @@ function createGrid() {
 
 createGrid()
 
+
+// Trying to store tetromineos in an object
 const i = {
-  default: [{row: 0, cell: 0}, {row: 1, cell: 0}, {row: 2, cell: 0}, {row: 3, cell: 0}],
+  default: [{row: 0, cell: 1}, {row: 0, cell: 2}, {row: 0, cell: 3}, {row: 0, cell: 4}],
   rotations: [{row: 1, cell: 1}, {row: 1, cell: 2}, {row: 1, cell: 3}, {row: 1, cell: 4}],
   class: ['i']
 }
 
 const l = {
   default: [{row: 0, cell: 0}, {row: 1, cell: 0}, { row: 2, cell: 0}, {row: 2, cell: 1}],
+  rotations: [
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}]
+  ],
   class: ['l']
 }
 
 const j = {
   default: [{row: 0, cell: 1}, {row: 1, cell: 1}, {row: 2, cell: 1}, {row: 2, cell: 0}],
+  rotations: [
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}]
+  ],
   class: ['j']
 }
 
 const t = {
   default: [{row: 0, cell: 1}, {row: 1, cell: 0}, {row: 1, cell: 1}, {row: 1, cell: 2}],
+  rotations: [
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}]
+  ],
   class: ['t']
 }
 
 const s = {
   default: [{row: 0, cell: 1}, {row: 0, cell: 2}, { row: 1, cell: 0}, {row: 1, cell: 1}],
+  rotations: [
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+  ],
   class: ['s']
 }
 
 
 const z = {
   default: [{row: 0, cell: 0}, {row: 0, cell: 1}, { row: 1, cell: 1}, {row: 1, cell: 2}],
+  rotations: [
+    [{row: 0, cell: 0}, {row: 0, cell:0}, {row: 0, cell: 0}, {row: 0, cell: 0}],
+  ],
   class: ['z']
 }
 
@@ -67,6 +90,7 @@ const o = {
   default: [{row: 0, cell: 0}, {row: 0, cell: 1}, { row: 1, cell: 0}, {row: 1, cell: 1}],
   class: ['o']
 }
+
 
 // const o = [{row: 0, cell: 0}, {row: 0, cell: 1}, { row: 1, cell: 0}, {row: 1, cell: 1}]
 // o.default.forEach(item => console.log(item)) - Logs Co-Ordinates for default position
@@ -77,36 +101,36 @@ const o = {
 // console.log(i.default[2].row, i.default[2].cell)
 // console.log(i.default[3].row, i.default[3].cell)
 
-const tetrominoes = {
-  i: {
-    default: [{row: 0, cell: 0}, {row: 1, cell: 0}, {row: 2, cell: 0}, {row: 3, cell: 0}],
-    class: ['i']
-  },
-  l: {
-    default: [{row: 0, cell: 0}, {row: 1, cell: 0}, { row: 2, cell: 0}, {row: 2, cell: 1}],
-    class: ['l']
-  },
-  j: {
-    default: [{row: 0, cell: 1}, {row: 1, cell: 1}, {row: 2, cell: 1}, {row: 2, cell: 0}],
-    class: ['j']
-  },
-  t: {
-    default: [{row: 0, cell: 1}, {row: 1, cell: 0}, {row: 1, cell: 1}, {row: 1, cell: 2}],
-    class: ['t']
-  },
-  s: {
-    default: [{row: 0, cell: 1}, {row: 0, cell: 2}, { row: 1, cell: 0}, {row: 1, cell: 1}],
-    class: ['s']
-  },
-  z: {
-    default: [{row: 0, cell: 0}, {row: 0, cell: 1}, { row: 1, cell: 1}, {row: 1, cell: 2}],
-    class: ['z']
-  },
-  o: {
-    default: [{row: 0, cell: 0}, {row: 0, cell: 1}, { row: 1, cell: 0}, {row: 1, cell: 1}],
-    class: ['o']
-  }
-}
+// const tetrominoes = {
+//   i: {
+//     default: [{row: 0, cell: 0}, {row: 1, cell: 0}, {row: 2, cell: 0}, {row: 3, cell: 0}],
+//     class: ['i']
+//   },
+//   l: {
+//     default: [{row: 0, cell: 0}, {row: 1, cell: 0}, { row: 2, cell: 0}, {row: 2, cell: 1}],
+//     class: ['l']
+//   },
+//   j: {
+//     default: [{row: 0, cell: 1}, {row: 1, cell: 1}, {row: 2, cell: 1}, {row: 2, cell: 0}],
+//     class: ['j']
+//   },
+//   t: {
+//     default: [{row: 0, cell: 1}, {row: 1, cell: 0}, {row: 1, cell: 1}, {row: 1, cell: 2}],
+//     class: ['t']
+//   },
+//   s: {
+//     default: [{row: 0, cell: 1}, {row: 0, cell: 2}, { row: 1, cell: 0}, {row: 1, cell: 1}],
+//     class: ['s']
+//   },
+//   z: {
+//     default: [{row: 0, cell: 0}, {row: 0, cell: 1}, { row: 1, cell: 1}, {row: 1, cell: 2}],
+//     class: ['z']
+//   },
+//   o: {
+//     default: [{row: 0, cell: 0}, {row: 0, cell: 1}, { row: 1, cell: 0}, {row: 1, cell: 1}],
+//     class: ['o']
+//   }
+// }
 
 // console.log(tetrominoes)
 // console.log(tetrominoes.i)
@@ -117,19 +141,37 @@ const tetrominoes = {
 // }
 
 const shapeHistory = []
+const tetrominoesList = [i, l, j, t, s, z, o]
+const randomShape = Math.floor(Math.random() * tetrominoesList.length)
 
 function generateShape() {
 
-  const tetrominoesList = [i, l, j, t, s, z, o]
-  const randomShape = Math.floor(Math.random() * tetrominoesList.length)
   // console.log(`RandomShape -->`,tetrominoesList[randomShape].default.map(item => item.row += 3))
-  tetrominoesList[randomShape].default.map(item => item.cell += 4)
-  shapeHistory.push(tetrominoesList[randomShape])
-  //return addTetro(tetrominoesList[randomShape]) // -  Run This to Generate a Random Terominoe
+  tetrominoesList[randomShape].default.map(item => {
+    if(tetrominoesList[randomShape] === tetrominoesList[0]) {
+      return item.cell += 2
+    } else {
+      return item.cell += 4
+    }
+  })
+  // return addTetro(tetrominoesList[randomShape]) // -  Run This to Generate a Random Terominoe
 }
 
-generateShape()
-console.log(shapeHistory)
+console.log(`Shape History --> `,shapeHistory)
+let currentShape = tetrominoesList[randomShape]
+
+// generateShape()
+
+
+// console.log(`Current Shape --> `,currentShape)
+
+// for(let i = 0; i < 3; i++) {
+//   generateShape()
+//   // Generates 3 Random shapes that could go into shape history and then pull the first in that list to be the next shape
+// }
+// console.log(`Shape History -->`,shapeHistory)
+
+// generateShape()
 
 // Array of a list of randomly generate Tetrominoes
 // for(let i = 0; i < 10; i++) {
@@ -139,14 +181,42 @@ console.log(shapeHistory)
 
 // generateShape()
 
+// Checks which element contains the class/state of block
+rows.forEach(row => {
+  // console.log(row)
+
+  // count how many row element contain the class of block
+  // console.log(row.classList)
+
+  row.childNodes.forEach((elem, index) => {
+    if(elem.classList.contains('block')) {
+      // console.log(elem) // - logs all element that contain block
+      // console.log(elem.dataset.column) // - Element position in column
+      // console.log(elem.parentNode.dataset.row) // - Element position in row
+      // console.log(elem.parentNode)
+    }
+  })
+})
+
+// Checks which element doesnt contain the class of block
+rows.forEach(item => {
+  item.childNodes.forEach(elem => {
+    if(!elem.classList.contains('block')) {
+      // console.log(elem)
+      // elem.style.background = 'red'
+    }
+  })
+})
+
+// Used for shape movement add shape to the grid when a keypress happens
 function addTetro(shape) {
   shape.default.forEach(item => {
     rows[item.row].childNodes[item.cell].classList.add('block', `${shape.class[0]}`)
   })
 }
 
-// addTetro(i)
 
+// Used for shape movement remove a shape to the grid when a keypress happens
 function removeTetro(shape) {
   shape.default.forEach(item => {
     // console.log(item.row, item.cell)
@@ -154,34 +224,35 @@ function removeTetro(shape) {
   })
 }
 
-// let currentShape = generateShape()
 
-
+// This drops the shape down the grip to the next available space
 function moveDown() {
 
   let count = 0
 
   const moveDownTimer = setInterval(() => {
 
-    if(i.default[0].row < gridHeight - 1 && i.default[1].row < gridHeight - 1 && i.default[2].row < gridHeight - 1 && i.default[3].row < gridHeight - 1) {
-      removeTetro(i)
-      i.default[0].row++
-      i.default[1].row++
-      i.default[2].row++
-      i.default[3].row++
-      addTetro(i)
-      console.log('Keep Running...')
+    if(currentShape.default[0].row < gridHeight - 1 && currentShape.default[1].row < gridHeight - 1 && currentShape.default[2].row < gridHeight - 1 && currentShape.default[3].row < gridHeight - 1) {
+      removeTetro(currentShape)
+      currentShape.default[0].row++
+      currentShape.default[1].row++
+      currentShape.default[2].row++
+      currentShape.default[3].row++
+      addTetro(currentShape)
+      // console.log('Keep Running...')
       count++
     }
 
-    if(count > 16) {
-      console.log('Running...')
-      console.log(grid[0])
-      console.log(`count -->`,count)
+    if(count > 18) {
+      // console.log('Running...')
+      // console.log(grid[0])
+      // console.log(`count -->`,count)
 
       // add set class here -------------------------
       // To add collison for blocks      
       //---------------------------------------------
+
+      // Set that shape in this space
 
       clearInterval(moveDownTimer)
     }
@@ -191,35 +262,44 @@ function moveDown() {
 
 }
 
-// moveDown()
+moveDown()
+
+// if the div contains a class of block dont allow anther element to pass through it.
+// Use a conditional if statement to check this
+// if classlist.contains('.block') === false dont go through
+
+console.log(cells.forEach(cell => cell.dataset > i.default[3].cell && !cell.classList.contains('block')))
 
 function enableKeyPress(event) {
   const key = event.keyCode
   // console.log('enable key press')
-  removeTetro(i)
+  // removeTetro(i) // only selecting the i element, needs to selected the random element
+  // e.g. removeTetro(tetrominoesList[randomShape])
+  removeTetro(currentShape)
 
-  if(key === 39 && i.default[0].cell % gridWidth !== gridWidth - 1 && i.default[1].cell % gridWidth !== gridWidth - 1 && i.default[2].cell % gridWidth !== gridWidth - 1 && i.default[3].cell % gridWidth !== gridWidth - 1) {
-    i.default[0].cell++
-    i.default[1].cell++
-    i.default[2].cell++
-    i.default[3].cell++
-  } else if(key === 37 && i.default[0].cell % gridWidth !== 0 && i.default[1].cell % gridWidth !== 0 && i.default[2].cell % gridWidth !== 0 && i.default[3].cell % gridWidth !== 0) {
-    i.default[0].cell--
-    i.default[1].cell--
-    i.default[2].cell--
-    i.default[3].cell--
-  } else if(key === 38 && i.default[0].row <= gridHeight && i.default[1].row <= gridHeight && i.default[2].row <= gridHeight && i.default[3].row <= gridHeight) {
-    i.default[0].row--
-    i.default[1].row--
-    i.default[2].row--
-    i.default[3].row--
-  } else if(key === 40 && i.default[0].row < gridHeight - 1 && i.default[1].row < gridHeight - 1 && i.default[2].row < gridHeight - 1 && i.default[3].row < gridHeight - 1) {
-    i.default[0].row++
-    i.default[1].row++
-    i.default[2].row++
-    i.default[3].row++
+  if(key === 39 && currentShape.default[0].cell % gridWidth !== gridWidth - 1 && currentShape.default[1].cell % gridWidth !== gridWidth - 1 && currentShape.default[2].cell % gridWidth !== gridWidth - 1 && currentShape.default[3].cell % gridWidth !== gridWidth - 1) {
+    currentShape.default[0].cell++
+    currentShape.default[1].cell++
+    currentShape.default[2].cell++
+    currentShape.default[3].cell++
+  } else if(key === 37 && currentShape.default[0].cell % gridWidth !== 0 && currentShape.default[1].cell % gridWidth !== 0 && currentShape.default[2].cell % gridWidth !== 0 && currentShape.default[3].cell % gridWidth !== 0) {
+    currentShape.default[0].cell--
+    currentShape.default[1].cell--
+    currentShape.default[2].cell--
+    currentShape.default[3].cell--
+  } else if(key === 38 && currentShape.default[0].row <= gridHeight && currentShape.default[1].row <= gridHeight && currentShape.default[2].row <= gridHeight && currentShape.default[3].row <= gridHeight) {
+    currentShape.default[0].row--
+    currentShape.default[1].row--
+    currentShape.default[2].row--
+    currentShape.default[3].row--
+  } else if(key === 40 && currentShape.default[0].row < gridHeight - 1 && currentShape.default[1].row < gridHeight - 1 && currentShape.default[2].row < gridHeight - 1 && currentShape.default[3].row < gridHeight - 1) {
+    currentShape.default[0].row++
+    currentShape.default[1].row++
+    currentShape.default[2].row++
+    currentShape.default[3].row++
   }
-  addTetro(i)
+  // addTetro(i) // only selecting the i element, needs to selected the random element
+  addTetro(currentShape)
 }
 
 document.addEventListener('keydown', enableKeyPress)
